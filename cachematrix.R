1d159a0fb6b52d748e1a02093ebb1dcd6362834c
germanscillato <71908758+germanscillato@users.noreply.github.com>
2020-09-26 17:30:11 -0300

    Add files via upload
    ProgrammingAssignment2 - R programming Course.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+##  Assumptions: the matrix supplied is always invertible.
+## Objetive: Calculate the inverse of a invertible matrix, and save compunting time with a cache method storage of previus calculations.
+
+
+# Create list with a function that define a matrix data, take matrix data, generate inv matrix, take inv matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) 
+    {m <<- solve}
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+
 }
 
-
-## Write a short comment describing this function
+## solve matrix inverse, with a previus verification of inv matrix stored in cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinv(m)
+  m
+          ## Return a matrix that is the inverse of 'x'
 }
